c893df8c31aeb9c344e36c5c320c37c0c773a03a
Brian T. <bptiburcio@gmail.com>
2024-07-11 11:37:12 -0400

    Adds get route for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
 // It's done when the `/` homepage route renders a list of all projects from the database.
+router.get("/", (req, res) => {
+    
+});
 
 module.exports = router;

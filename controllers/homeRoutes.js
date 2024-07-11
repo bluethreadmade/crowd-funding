f14dbd26b629e13848ea8e284c7e7d384d9cbc8a
Brian T. <bptiburcio@gmail.com>
2024-07-11 12:03:40 -0400

    Add home router to controllers/index.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { User, Project } = require('../models');
 
 // It's done when the `/` homepage route renders a list of all projects from the database.
 router.get('/', (req, res) => {
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@
 const router = require('express').Router();
 // Import the index.js from 'api' folder
 const apiRoutes = require('./api');
+const homeRouter = require('./homeRoutes.js');
 
 // When a request is made to the /api route, it will be directed to the index.js in the 'api' folder.
 router.use('/api', apiRoutes);
+router.use('/', homeRouter);
 
 module.exports = router;
